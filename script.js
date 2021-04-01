2abf15b4695bc843264ab948d4c5df22e85023b7
Cesar-Felizardo <70537620+Cesar-Felizardo@users.noreply.github.com>
2021-04-01 17:25:17 -0300

    Atualização com @media

---
+++ script.js
@@ -0,0 +1,40 @@
+function mudarImg() {
+    document.getElementById("mudarImg").src="Dourado500.jpg";
+}
+function img1() {
+    document.getElementById("mudarImg").src="Frosen500.jpg";
+}
+function img2() {
+    document.getElementById("mudarImg").src="FreeFire500.jpg";
+}
+function img3() {
+    document.getElementById("mudarImg").src="BodasFlores500.jpg";
+}
+function img4() {
+    document.getElementById("mudarImg").src="Kamila600.jpg";
+}
+function img5() {
+    document.getElementById("mudarImg").src="Larissa600.jpg";
+}
+function img6() {
+    document.getElementById("mudarImg").src="Miniuns600.jpg";
+}
+function img7() {
+    document.getElementById("mudarImg").src="Unicornio600.jpg";
+} 
+function img8() {
+    document.getElementById("mudarImg").src="NsAparecida600.jpg" ;
+} 
+function img9() {
+    document.getElementById("mudarImg").src="MariaVitoria600.jpg";
+} 
+function img10() {
+    document.getElementById("mudarImg").src="Lucas600.jpg";
+} 
+function img11() {
+    document.getElementById("mudarImg").src="OsIncriveis400.jpg";
+} 
+function img12() {
+    document.getElementById("mudarImg").src="Heloisa400.jpg";
+} 
+
