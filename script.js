0e61fddebb627d117a78fe43b0f8ea7247f44fb8
Your Name <you@example.com>
2021-06-26 14:47:12 -0300

    Lógica Js...

--- script.js
+++ script.js
@@ -1,40 +1,229 @@
+
+/* 
 function mudarImg() {
     document.getElementById("mudarImg").src="Dourado500.jpg";
-}
+    document.getElementsByClassName('imagens').style.border = "2px double red" 
+} */
+
 function img1() {
-    document.getElementById("mudarImg").src="Frosen500.jpg";
+    var img1 = document.getElementById("mudarImg")
+    img1.src="Frosen500.jpg";
+    //select on off
+    var imgFrosen = document.getElementById("imgFrosen")
+    imgFrosen.style.border ="3px double pink"
+    imgBodasFlores.style.border = '3px double #000'
+    imgFire.style.border = '3px double #000'
+    imgKamila.style.border = '3px double #000'
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 }
 function img2() {
-    document.getElementById("mudarImg").src="FreeFire500.jpg";
+    var img2 = document.getElementById("mudarImg")
+    img2.src="FreeFire500.jpg";
+    //select on off
+    var imgFire = document.getElementById("imgFire")
+    imgFire.style.border ="3px double pink"
+    imgFrosen.style.border ="3px double #000"
+    imgBodasFlores.style.border = '3px double #000'
+    imgKamila.style.border = '3px double #000'
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
+   
 }
 function img3() {
-    document.getElementById("mudarImg").src="BodasFlores500.jpg";
+    var img3 = document.getElementById("mudarImg")
+    img3.src="BodasFlores500.jpg";
+    //select on off
+    var imgBodasFlores = document.getElementById("imgBodasFlores")
+    imgBodasFlores.style.border= "3px double pink"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgKamila.style.border = '3px double #000'
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
+    
 }
 function img4() {
-    document.getElementById("mudarImg").src="Kamila600.jpg";
+    var img4 = document.getElementById("mudarImg")
+    img4.src="Kamila600.jpg";
+    //select on off
+    var imgKamila = document.getElementById("imgKamila")
+    imgKamila.style.border = "3px double pink"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 }
 function img5() {
-    document.getElementById("mudarImg").src="Larissa600.jpg";
+    var img5 = document.getElementById("mudarImg")
+    img5.src="Larissa600.jpg";
+    //select on off
+    var imgLarissa = document.getElementById("imgLarissa")
+    imgLarissa.style.border = "3px double pink"
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
+    
 }
 function img6() {
-    document.getElementById("mudarImg").src="Miniuns600.jpg";
+    var img6 = document.getElementById("mudarImg")
+    img6.src="Miniuns600.jpg";
+    //select on off
+    var imgMiniuns = document.getElementById("imgMiniuns")
+    imgMiniuns.style.border = "3px double pink"
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
+
 }
 function img7() {
-    document.getElementById("mudarImg").src="Unicornio600.jpg";
+    var img7 = document.getElementById("mudarImg")
+    img7.src="Unicornio600.jpg";
+    //select on off
+    var imgUnicornio = document.getElementById("imgUnicornio")
+    imgUnicornio.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
+    
 } 
 function img8() {
-    document.getElementById("mudarImg").src="NsAparecida600.jpg" ;
+    var img8 = document.getElementById("mudarImg")
+    img8.src="NsAparecida600.jpg" ;
+    //select on off
+    var imgNsAparecida = document.getElementById("imgNsAparecida")
+    imgNsAparecida.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 } 
 function img9() {
-    document.getElementById("mudarImg").src="MariaVitoria600.jpg";
+    var img9 = document.getElementById("mudarImg")
+    img9.src="MariaVitoria600.jpg";
+    //select on off
+    var imgMariaVitoria = document.getElementById("imgMariaVitoria")
+    imgMariaVitoria.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 } 
 function img10() {
-    document.getElementById("mudarImg").src="Lucas600.jpg";
+    var img10 = document.getElementById("mudarImg")
+    img10.src="Lucas600.jpg";
+    //select on off
+    var imgLucas = document.getElementById("imgLucas")
+    imgLucas.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 } 
 function img11() {
-    document.getElementById("mudarImg").src="OsIncriveis400.jpg";
+    var img11 = document.getElementById("mudarImg")
+    img11.src="OsIncriveis400.jpg";
+    //select on off
+    var imgOsIncriveis = document.getElementById("imgOsIncriveis")
+    imgOsIncriveis.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgHeloisa.style.border = '3px double #000'
 } 
 function img12() {
-    document.getElementById("mudarImg").src="Heloisa400.jpg";
+    var img12 = document.getElementById("mudarImg")
+    img12.src="Heloisa400.jpg";
+    //select on off
+    var imgHeloisa = document.getElementById("imgHeloisa")
+    imgHeloisa.style.border = '3px double pink'
+    imgKamila.style.border = "3px double #000"
+    imgBodasFlores.style.border= "3px double #000"
+    imgFire.style.border ="3px double #000"
+    imgFrosen.style.border ="3px double #000"
+    imgLarissa.style.border = '3px double #000'
+    imgMiniuns.style.border = '3px double #000'
+    imgUnicornio.style.border = '3px double #000'
+    imgNsAparecida.style.border = '3px double #000'
+    imgMariaVitoria.style.border = '3px double #000'
+    imgLucas.style.border = '3px double #000'
+    imgOsIncriveis.style.border = '3px double #000'
 } 
 
